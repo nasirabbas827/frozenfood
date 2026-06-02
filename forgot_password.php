bd96c732eaa9fa393def039579b3d502d3fe7798
NASIR ABBAS <85347981+nasirabbas827@users.noreply.github.com>
2026-06-02 11:31:32 +0500

    security: remove exposed credentials

--- forgot_password.php
+++ forgot_password.php
@@ -47,7 +47,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
                         $mail->Host = 'smtp.gmail.com';
                         $mail->SMTPAuth = true;
                         $mail->Username = 'nasiryt.827@gmail.com'; // Your Gmail address
-                        $mail->Password = 'mtvp ruzp aqfu tfxt'; // App password from Gmail
+                        $mail->Password = "YOUR_OWN_API_KEY"; // App password from Gmail
                         $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
                         $mail->Port = 587;
 
